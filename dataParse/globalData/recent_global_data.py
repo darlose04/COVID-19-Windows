8ddc11a4cc9f657fc4d92cc721200d42fc103ae5
Zach Smith <zsmith126028@gmail.com>
2020-11-13 14:01:25 -0600

    create script that makes the csvs to get pushed into the db

---
+++ dataParse/globalData/recent_global_data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+csv_path = "E:/Programming/Projects/COVID-19-Windows/covidData/COVID-19/csse_covid_19_data/csse_covid_19_time_series/"
+
+global_cases = pd.read_csv(csv_path + 'time_series_covid19_confirmed_global.csv')
+global_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_global.csv')
+
+confirmed_dates = global_cases.columns[4:]
+death_dates = global_deaths.columns[4:]
+
+def create_csv(data, dates, csv_name):
+    columns = ['ID']
+
+    for header in dates:
+        columns.append(header)
+
+    id_arr = []
+    increment = 1
+
+    while increment <= data.shape[0]:
+        id_arr.append(increment)
+        increment += 1
+
+    col_arr = []
+    count = 1
+
+    while count <= data.shape[0]:
+        sub_arr = []
+        sub_arr.append(id_arr.pop(0))
+
+        for header in dates:
+            sub_arr.append(data[header].pop(count - 1))
+
+        col_arr.append(sub_arr)
+
+        count += 1
+
+    csv = csv_name + '.csv'
+    csv_name = pd.DataFrame(col_arr, columns=columns,)
+    csv_name.to_csv(csv)
+
+create_csv(global_cases, confirmed_dates, 'global_cases')
+create_csv(global_deaths, death_dates, 'global_deaths')
